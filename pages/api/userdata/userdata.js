aba49d21b64a36fdcf00699ee80dd6b8518d3eaa
IR0ng <zhouirong0420@gmail.com>
2022-05-31 14:24:12 +0800

    update

--- pages/api/userdata/userdata.js
+++ pages/api/userdata/userdata.js
@@ -5,35 +5,35 @@ export default async function handler(req, res) {
   const { name, email, password } = req.body;
   const newData = { name, email, password };
   const resData = await UserModel.userSignUp(newData);
-  req.body = {
+  
+  res.status(200).send({
     status : "ok",
     id : resData.id
-  }
-  res.status(200).send(req.body)
+  })
 
   }else if (req.method === "GET"){
     const { id } = req.body
     const resData = await UserModel.findUserData(id);
-    req.body = {
+
+    res.status(200).send({
       status : "ok",
       userdata: resData
-    }
-    res.status(200).send(req.body)
+    })
 
   }else if (req.method === "PATCH") {
-    const resData = await UserModel.Update(req.body);
-    req.body = {
+    await UserModel.Update(req.body);
+
+    res.status(200).send({
       status : "ok",
-    }
-    res.status(200).send(req.body)
+    })
 
   }else if (req.method === "DELETE"){
     const { id } = req.body
     const resData = await UserModel.deleteUserData(id);
-    req.body = {
+
+    res.status(200).send({
       status : "ok",
       Deleted : resData
-    }
-    res.status(200).send(req.body)
+    })
   }
 }
